571f68c04d617e8b96b777ba274f10245f4004f8
kusa10mm <hirotoshi.kusano@bizreach.co.jp>
2021-07-19 15:58:35 +0900

    yarn v2の環境作り直し。helloworld内でaxiosが使えてisbotが使えないを目指す

--- packages/helloworld/src/index.ts
+++ packages/helloworld/src/index.ts
@@ -1,8 +1,11 @@
 // import {getMessage} from "not-util";
-import {getMessage} from "careertrek-util";
+// import {getMessage} from "careertrek-util";
+import axios from "axios";
+import isBot from 'isbot';
 
 const main = () => {
-    console.log(getMessage);
+    console.log(axios);
+    console.log(isBot);
     console.log("hello world");
 }
 
